648beecd77caf8c3ac2b9cd405a4204f9ffadd46
apex-glyptodont <caffeinateddeveloper@gmail.com>
2018-03-10 01:37:34 -0500

    fixed up some things, havent tested create PollSubset, find with id isnt working either

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const server = new grpc.Server();
 
 let polls = [
   {
-    id: 123, 
-    question: "What is a poll question?",
-    numUpvotes: 4 ,
+    id: 1, 
+    question: "This is default poll",
+    numUpvotes: 1,
     polleeCanAddOptions: true,
     polleeCanMultiVote: true,
     polleeCanComment: true
@@ -21,39 +21,59 @@ server.addService(pollsProto.polls.PollService.service, {
   list: (call, cb) => {
     cb(null, polls);
   },
-  insert: (call, cb) => { 
+  create: (call, cb) => { 
+    // havent updated with oneof trick to differentiate between default and unsent values..
     let poll = call.request;
     if(!isValid(poll)) return cb({
       code: grpc.status.INVALID_ARGUMENT,
-      details: 'Not found'
+      details: 'You must include a poll question'
     });
     polls.push(poll);
     cb(null, poll)
   },
-  listOne: (call, cb) => cb(null, polls.find( p => p.id === call.request.id)),
-  updateOne: (call, cb) => {
-    if(!call.request.id) return cb(null, {}); 
-
+  find: (call, cb) => {
     const idKey = call.request.id;
-    console.log(idKey);
-    call.request.id = null;
-    let poll = polls.find( p => p.id === call.request[idKey]);
-    Object.keys(call.request).filter((key) => {
+    console.log(call.request);
+    if(!idKey) return cb({
+      code: grpc.status.INVALID_ARGUMENT,
+      details: 'You must include a poll id'
+    });
+    const id = call.request[idKey + '_val'];
+    const poll = polls.find( p => p.id === id );
+    if(!poll) return cb({
+      code: grpc.status.NOT_FOUND,
+      details: 'Poll not found!'
+    });
+    cb(null, poll)
+  },
+  update: (call, cb) => {
+    const pollReq = call.request;
+    const idKey = pollReq.id;
+    if(idKey === null) return cb({         // this may have to change to check for null
+      code: grpc.status.INVALID_ARGUMENT,
+      details: 'You must include a poll id'
+    }); 
+    const id = pollReq[idKey + '_val'];
+    pollReq.id = null;
+    let poll = polls.find( p => p.id === pollReq[idKey]); 
+    Object.keys(pollReq).filter((key) => {                     // only look at fields that were included in request
       console.log(key);
       return key !== null
-    }).map((existingKey) => {
+    }).map((existingKey) => {              	    	       // update poll obj so those fields's values are updated 
       console.log(existingKey);
-      poll[existingKey] = call.request[existingKey + '_val'];
+      poll[existingKey] = pollReq[existingKey + '_val']; 
     });
 
     cb(null, poll);
   },
-  deleteOne: (call, cb) => {
-    if(!call.request.id) return cb({ code: grpc.status.NOT_FOUND, details: 'Not found'});
-    const id = call.request.id;
+  delete: (call, cb) => {
+    const pollReq = call.request;
+    const idKey = pollReq.id;
+    if(!idKey) return cb({ code: grpc.status.INVALID_ARGUMENT, details: 'You must include a poll id'});
+    const id = pollReq[idKey + '_val'];
     let poll = polls.find( p => p.id === id );
     if(!poll) return cb({ code: grpc.status.NOT_FOUND, details: 'NOT found'});
-    polls = polls.filter( p => p.id !== id );  //lolz
+    polls = polls.filter( p => p.id !== id );
     cb(null, {});
   }
 });
